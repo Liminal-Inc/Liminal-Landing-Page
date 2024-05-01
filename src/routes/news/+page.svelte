f3bc62f97b93f7d0aeb8fc06c7dfbb727c36f19c
Dane <dane@liminalbios.com>
2024-04-30 20:19:00 -0400

    Added first blog post - lab state vs metadata

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,8 +1,12 @@
 <script>
-
+    import { Button } from "flowbite-svelte";
 </script>
 
 <div class="retro-font text-white">
     <h1 class="text-white">News</h1>
-    <p>Status: <span class="text-orange-500">Pending</span></p>
+    <p class="mt-2 mb-2">April 2024:
+        <Button color="primary" size="sm" outline href="/news/labstate"><span class="text-green-600">Lab State vs. Metadata</span></Button>
+        
+    </p>
+    <p>Next Up: <span class="text-orange-500">Pending</span></p>
 </div>
