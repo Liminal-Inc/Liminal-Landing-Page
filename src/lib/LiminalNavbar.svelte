28dd1605d13a3c243725e7ea69f9c3dad9645edf
Dane <dane@liminalbios.com>
2024-06-15 10:59:44 -0400

    Updated broken news link

--- src/lib/LiminalNavbar.svelte
+++ src/lib/LiminalNavbar.svelte
@@ -33,19 +33,21 @@
 
 <Navbar class="bg-purple-300 px-4 py-2.5 retro-font">
     <div class="container mx-auto flex flex-wrap justify-between items-center">
-      <a href="/" class="flex items-center">
-        <img src={logoURL} class="mr-3 h-6 sm:h-9" alt="Logo" />
-        <span class="self-center text-xl font-semibold whitespace-nowrap text-black hover:text-purple">Liminal Inc</span>
-      </a>
-      <div class="flex md:order-2">
-        <a href="/details">
-            <span class="text-black text-md md:text-base whitespace-nowrap font-bold">App Details</span>
+      <div class="flex">
+        <a href="/" class="flex items-center">
+          <img src={logoURL} class="mr-3 h-6 sm:h-9" alt="Logo" />
+          <span class="self-center text-xl font-semibold whitespace-nowrap text-black hover:text-purple">Liminal Inc</span>
         </a>
       </div>
-      <div class="flex gap-3">
+      <div class="flex gap-3 justify-center">
         <a href="/team" class="text-black hover:text-gray-300 outline-dashed outline-offset-2 mr-1">Team</a>
         <a href="/news" class="text-black hover:text-gray-300 outline-dashed outline-offset-2 mr-1">News</a>
         <a href="/research" class="text-black hover:text-gray-300 outline-dashed outline-offset-2">Research</a>
+        </div>
+      <div class="flex">
+        <a href="/details">
+            <span class="text-black text-md md:text-base whitespace-nowrap font-bold">App Details</span>
+        </a>
       </div>
     </div>
   </Navbar>
--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -14,13 +14,13 @@
     <hr>
         <h1 class="text-white text-lg mt-2">Blogs Only</h1>
         <p class="mt-2 mb-2">June 2024:
-            <Button color="primary" size="sm" outline href="/news/labknowledge"><span class="text-green-600">Lab Knowledge</span></Button>
+            <Button color="primary" size="sm" outline href="labknowledge"><span class="text-green-600">Lab Knowledge</span></Button>
         </p>
         <p class="mt-2 mb-2">May 2024:
-            <Button color="primary" size="sm" outline href="/news/generaltools"><span class="text-green-600">General Tools</span></Button>
+            <Button color="primary" size="sm" outline href="generaltools"><span class="text-green-600">General Tools</span></Button>
         </p>
         <p class="mt-2 mb-2">April 2024:
-            <Button color="primary" size="sm" outline href="/news/labstate"><span class="text-green-600">Lab State vs. Metadata</span></Button>
+            <Button color="primary" size="sm" outline href="labstate"><span class="text-green-600">Lab State vs. Metadata</span></Button>
         </p>
         <p>Next Up: <span class="text-orange-500">Pending</span></p>
     </div>
