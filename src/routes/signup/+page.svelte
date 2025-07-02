b36a7cbe2cf93a5b8e50cdd3ec9ebe25ed07cd37
ddeemer <ddeemer@purdue.edu>
2025-07-02 13:52:49 -0400

    Better loading of hubspot form

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,24 +1,33 @@
+<script>
+	import { onMount } from 'svelte';
 
-<script src="https://js.hsforms.net/forms/embed/48817868.js" defer>
+	onMount(() => {
+		if (window.hbspt) {
+			window.hbspt.forms.create({
+				region: "na1",
+				portalId: "48817868",
+				formId: "0e6b6238-d9b8-49c4-85a4-30b500c43cac",
+				target: "#hubspot-form"
+			});
+		} else {
+			const script = document.createElement('script');
+			script.src = 'https://js.hsforms.net/forms/v2.js';
+			script.onload = () => {
+				window.hbspt.forms.create({
+					region: "na1",
+					portalId: "48817868",
+					formId: "0e6b6238-d9b8-49c4-85a4-30b500c43cac",
+					target: "#hubspot-form"
+				});
+			};
+			document.body.appendChild(script);
+		}
+	});
 </script>
 
 <section class="min-h-screen bg-black text-white px-4 py-12">
 	<div class="max-w-3xl mx-auto bg-white text-black p-8">
 		<h1 class="text-3xl font-bold mb-6">Join Liminal</h1>
-
-		<div class="aspect-video w-full">
-			<!-- <iframe
-				title="Sign up"
-				src="https://docs.google.com/forms/d/e/1FAIpQLSd2gtATL3ifoL3cg_IBaxFpP96MhLZY2uy9zVTxOUwRiPcXwQ/viewform?embedded=true"
-				width="640"
-				height="2335"
-				frameborder="0"
-				marginheight="0"
-				marginwidth="0">Loading…</iframe
-			> -->
-				<script src="https://js.hsforms.net/forms/embed/48817868.js" defer>
-</script>
-			<div class="hs-form-frame" data-region="na1" data-form-id="0e6b6238-d9b8-49c4-85a4-30b500c43cac" data-portal-id="48817868"></div>
-		</div>
+		<div id="hubspot-form" class="w-full"></div>
 	</div>
 </section>
