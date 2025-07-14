54dd3cb786d79010986bf3fcbeb16671ea9d01f6
ddeemer <ddeemer@purdue.edu>
2025-07-14 08:58:18 -0400

    New HubSpot form

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -2,32 +2,23 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		if (window.hbspt) {
-			window.hbspt.forms.create({
-				region: "na1",
-				portalId: "48817868",
-				formId: "0e6b6238-d9b8-49c4-85a4-30b500c43cac",
-				target: "#hubspot-form"
-			});
-		} else {
-			const script = document.createElement('script');
-			script.src = 'https://js.hsforms.net/forms/v2.js';
-			script.onload = () => {
-				window.hbspt.forms.create({
-					region: "na1",
-					portalId: "48817868",
-					formId: "0e6b6238-d9b8-49c4-85a4-30b500c43cac",
-					target: "#hubspot-form"
-				});
-			};
-			document.body.appendChild(script);
-		}
+		const script = document.createElement('script');
+		script.src = 'https://js.hsforms.net/forms/embed/48817868.js';
+		script.defer = true;
+		document.body.appendChild(script);
 	});
 </script>
 
-<section class="min-h-screen bg-black text-white px-4 py-12">
-	<div class="max-w-3xl mx-auto bg-white text-black p-8">
-		<h1 class="text-3xl font-bold mb-6">Join Liminal</h1>
-		<div id="hubspot-form" class="w-full"></div>
+<section class="min-h-screen bg-white px-4 py-12">
+	<div class="max-w-2xl mx-auto">
+		<h1 class="text-2xl font-bold mb-6">Sign up for Liminal</h1>
+
+		<!-- HubSpot Form Container -->
+		<div
+			class="hs-form-frame"
+			data-region="na1"
+			data-form-id="0e6b6238-d9b8-49c4-85a4-30b500c43cac"
+			data-portal-id="48817868"
+		/>
 	</div>
 </section>
